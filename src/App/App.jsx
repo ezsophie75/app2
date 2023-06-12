7d886b32f7cc04f46166e19e2fac83db289b3070
ezsophie75 <sophiphi2000@yahoo.fr>
2023-06-12 12:57:19 +0100

    bouton

---
+++ src/App/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Button from './Components/ui/Button/Button'
+
+function App() {
+  return (
+    <div className="App">
+      <Button></Button>
+      <Button></Button>
+    </div>
+  );
+}
+
+export default App;
