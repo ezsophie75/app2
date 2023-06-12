e1420618d3d610b9d399c72076a14e34489fe60a
ezsophie75 <sophiphi2000@yahoo.fr>
2023-06-12 16:17:03 +0100

    exercice 1 useEffect

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,9 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Button from './Components/ui/Button/Button'
 
 function App() {
   const [counter,setcounter] = useState(0) 
+  useEffect(()=> {
+    console.log('value post setcounter', counter)
+  }, [counter])
+  useEffect(() => {
+  console.log('creation du composant set des etats initiaux');
+  setcounter(1)
+  }, [])
   console.log('rendu')
   return (
     <div className="App">
